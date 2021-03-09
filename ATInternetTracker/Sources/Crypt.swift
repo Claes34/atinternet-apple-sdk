05bf8a80570be8abf9bb6734644f2c808b4ef056
Frédéric Mallet <frederic.mallet@idean.com>
2021-03-09 14:13:08 +0100

    replace CryptoKit by CommonCrypto

--- ATInternetTracker/Sources/Crypt.swift
+++ ATInternetTracker/Sources/Crypt.swift
@@ -30,9 +30,6 @@ SOFTWARE.
 //  Tracker
 //
 import Foundation
-#if canImport(CryptoKit)
-import CryptoKit
-#endif
 
 class Crypt: NSObject {
     
@@ -54,9 +51,7 @@ class Crypt: NSObject {
         }
         
         #if os(iOS) && !AT_EXTENSION
-        if #available(iOS 13.0, *) {
-            return encryption(data: data)
-        }
+        return encryption(data: data)
         #elseif os(watchOS)
         if #available(watchOS 6.0, *) {
             return encryption(data: data)
@@ -73,9 +68,7 @@ class Crypt: NSObject {
     
     func decrypt(data: String) -> String? {
         #if os(iOS) && !AT_EXTENSION
-        if #available(iOS 13.0, *) {
-            return decryption(data: data)
-        }
+        return decryption(data: data)
         #elseif os(watchOS)
         if #available(watchOS 6.0, *) {
             return decryption(data: data)
@@ -88,127 +81,36 @@ class Crypt: NSObject {
         
         return data
     }
-    
-    @available(iOS 13.0, *)
-    @available(tvOS 13.0, *)
-    @available(watchOS 6, *)
+
     private func encryption(data: String) -> String? {
-        let key = getKey()
-        guard key != nil else { return nil }
+        let iv = AES256.randomIv()
 
-        let encryptedData = try? AES.GCM.seal(data.data(using: .utf8)!, using: key!).combined
-
-        guard let sealData = encryptedData else { return nil }
+        guard let key = getKey(),
+            let aes = try? AES256(key: key, iv: iv),
+            let sealData = try? aes.encrypt(data.data(using: .utf8)!) else { return nil }
 
         return sealData.base64EncodedString()
     }
-    
-    @available(iOS 13.0, *)
-    @available(tvOS 13.0, *)
-    @available(watchOS 6, *)
+
     private func decryption(data: String) -> String? {
-                
-        let decoded = Data(base64Encoded: data)
-        guard decoded != nil else { return data }
+        guard let decoded = Data(base64Encoded: data) else {
+            return data
+        }
 
         do {
-            let sealedBoxRestored = try? AES.GCM.SealedBox(combined: decoded!)
-            guard sealedBoxRestored != nil else { return data }
+            let iv = AES256.randomIv()
 
-            let key = getKey()
-            guard key != nil else { return data }
+            guard let key = getKey() else { return data }
+            let aes = try AES256(key: key, iv: iv)
 
-            return try String(data: AES.GCM.open(sealedBoxRestored!, using: key!), encoding: .utf8)
+            return try String(data: aes.decrypt(decoded),
+                              encoding: .utf8)
         }
         catch { return data }
     }
-    
-    @available(iOS 13.0, *)
-    @available(tvOS 13.0, *)
-    @available(watchOS 6, *)
-    private func getKey() -> SymmetricKey? {
 
-        var query = [kSecClass: kSecClassGenericPassword,
-                     kSecAttrAccount: Crypt.ACCOUNT,
-                     kSecUseDataProtectionKeychain: true,
-                     kSecReturnData: true] as [String: Any]
-
-        var item: CFTypeRef?
-        if SecItemCopyMatching(query as CFDictionary, &item) == errSecSuccess {
-            if let optData = item as? Data {
-                return SymmetricKey(data: optData)
-            }
-            return nil
-        }
-
-        let key = SymmetricKey(size: .bits256)
-        query = [kSecClass: kSecClassGenericPassword,
-                 kSecAttrAccount: Crypt.ACCOUNT,
-                     kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
-                     kSecUseDataProtectionKeychain: true,
-                     kSecValueData: key.rawRepresentation] as [String: Any]
-
-        _ = SecItemAdd(query as CFDictionary, nil)
-        return key
-    }
-}
-
-@available(iOS 13.0, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6, *)
-extension SymmetricKey: CustomStringConvertible {
-
-    public var description: String {
-        return self.rawRepresentation.withUnsafeBytes { bytes in
-            return "Key representation contains \(bytes.count) bytes."
-        }
-    }
-
-    var rawRepresentation: Data {
-        return self.withUnsafeBytes { bytes in
-            let cfdata = CFDataCreateWithBytesNoCopy(nil, bytes.baseAddress?.assumingMemoryBound(to: UInt8.self), bytes.count, kCFAllocatorNull)
-            return ((cfdata as NSData?) as Data?) ?? Data()
-        }
-    }
-}
-
-@available(iOS 13.0, *)
-@available(tvOS 13.0, *)
-@available(watchOS 6, *)
-struct SymmetricKeyStore {
-
-    /// Stores a CryptoKit key in the keychain as a generic password.
-    func storeKey(_ key: SymmetricKey, account: String) -> SymmetricKey? {
-
-        // Treat the key data as a generic password.
-        let query = [kSecClass: kSecClassGenericPassword,
-                     kSecAttrAccount: account,
-                     kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
-                     kSecUseDataProtectionKeychain: true,
-                     kSecValueData: key.rawRepresentation] as [String: Any]
-
-        // Add the key data.
-        _ = SecItemAdd(query as CFDictionary, nil)
-        return readKey(account: account)
-    }
-
-    /// Reads a CryptoKit key from the keychain as a generic password.
-    func readKey(account: String) -> SymmetricKey? {
-
-        // Seek a generic password with the given account.
-        let query = [kSecClass: kSecClassGenericPassword,
-                     kSecAttrAccount: account,
-                     kSecUseDataProtectionKeychain: true,
-                     kSecReturnData: true] as [String: Any]
-
-        // Find and cast the result as data.
-        var item: CFTypeRef?
-        if SecItemCopyMatching(query as CFDictionary, &item) == errSecSuccess {
-            if let optData = item as? Data {
-                return SymmetricKey(data: optData)
-            }
-        }
-
-        return nil
+    private func getKey() -> Data? {
+        let salt = AES256.randomSalt()
+        return try? AES256.createKey(password: Crypt.ACCOUNT.data(using: .utf8)!, salt: salt)
     }
 }

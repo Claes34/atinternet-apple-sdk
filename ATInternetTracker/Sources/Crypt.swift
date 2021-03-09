03d57cf5a4a3f05c68a2160402cca3b2c920002b
Frédéric Mallet <frederic.mallet@idean.com>
2021-03-09 09:51:05 +0100

    Remove canImport

--- ATInternetTracker/Sources/Crypt.swift
+++ ATInternetTracker/Sources/Crypt.swift
@@ -30,9 +30,7 @@ SOFTWARE.
 //  Tracker
 //
 import Foundation
-//#if canImport(CryptoKit)
-//import CryptoKit
-//#endif
+import CryptoKit
 
 class Crypt: NSObject {
     
@@ -93,15 +91,14 @@ class Crypt: NSObject {
     @available(tvOS 13.0, *)
     @available(watchOS 6, *)
     private func encryption(data: String) -> String? {
-        return nil
-//        let key = getKey()
-//        guard key != nil else { return nil }
-//
-//        let encryptedData = try? AES.GCM.seal(data.data(using: .utf8)!, using: key!).combined
-//
-//        guard let sealData = encryptedData else { return nil }
-//
-//        return sealData.base64EncodedString()
+        let key = getKey()
+        guard key != nil else { return nil }
+
+        let encryptedData = try? AES.GCM.seal(data.data(using: .utf8)!, using: key!).combined
+
+        guard let sealData = encryptedData else { return nil }
+
+        return sealData.base64EncodedString()
     }
     
     @available(iOS 13.0, *)
@@ -112,105 +109,104 @@ class Crypt: NSObject {
         let decoded = Data(base64Encoded: data)
         guard decoded != nil else { return data }
 
-        return data
-//        do {
-//            let sealedBoxRestored = try? AES.GCM.SealedBox(combined: decoded!)
-//            guard sealedBoxRestored != nil else { return data }
-//
-//            let key = getKey()
-//            guard key != nil else { return data }
-//
-//            return try String(data: AES.GCM.open(sealedBoxRestored!, using: key!), encoding: .utf8)
-//        }
-//        catch { return data }
+        do {
+            let sealedBoxRestored = try? AES.GCM.SealedBox(combined: decoded!)
+            guard sealedBoxRestored != nil else { return data }
+
+            let key = getKey()
+            guard key != nil else { return data }
+
+            return try String(data: AES.GCM.open(sealedBoxRestored!, using: key!), encoding: .utf8)
+        }
+        catch { return data }
     }
     
-//    @available(iOS 13.0, *)
-//    @available(tvOS 13.0, *)
-//    @available(watchOS 6, *)
-//    private func getKey() -> SymmetricKey? {
-//
-//        var query = [kSecClass: kSecClassGenericPassword,
-//                     kSecAttrAccount: Crypt.ACCOUNT,
-//                     kSecUseDataProtectionKeychain: true,
-//                     kSecReturnData: true] as [String: Any]
-//
-//        var item: CFTypeRef?
-//        if SecItemCopyMatching(query as CFDictionary, &item) == errSecSuccess {
-//            if let optData = item as? Data {
-//                return SymmetricKey(data: optData)
-//            }
-//            return nil
-//        }
-//
-//        let key = SymmetricKey(size: .bits256)
-//        query = [kSecClass: kSecClassGenericPassword,
-//                 kSecAttrAccount: Crypt.ACCOUNT,
-//                     kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
-//                     kSecUseDataProtectionKeychain: true,
-//                     kSecValueData: key.rawRepresentation] as [String: Any]
-//
-//        _ = SecItemAdd(query as CFDictionary, nil)
-//        return key
-//    }
+    @available(iOS 13.0, *)
+    @available(tvOS 13.0, *)
+    @available(watchOS 6, *)
+    private func getKey() -> SymmetricKey? {
+
+        var query = [kSecClass: kSecClassGenericPassword,
+                     kSecAttrAccount: Crypt.ACCOUNT,
+                     kSecUseDataProtectionKeychain: true,
+                     kSecReturnData: true] as [String: Any]
+
+        var item: CFTypeRef?
+        if SecItemCopyMatching(query as CFDictionary, &item) == errSecSuccess {
+            if let optData = item as? Data {
+                return SymmetricKey(data: optData)
+            }
+            return nil
+        }
+
+        let key = SymmetricKey(size: .bits256)
+        query = [kSecClass: kSecClassGenericPassword,
+                 kSecAttrAccount: Crypt.ACCOUNT,
+                     kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
+                     kSecUseDataProtectionKeychain: true,
+                     kSecValueData: key.rawRepresentation] as [String: Any]
+
+        _ = SecItemAdd(query as CFDictionary, nil)
+        return key
+    }
 }
 
-//@available(iOS 13.0, *)
-//@available(tvOS 13.0, *)
-//@available(watchOS 6, *)
-//extension SymmetricKey: CustomStringConvertible {
-//
-//    public var description: String {
-//        return self.rawRepresentation.withUnsafeBytes { bytes in
-//            return "Key representation contains \(bytes.count) bytes."
-//        }
-//    }
-//
-//    var rawRepresentation: Data {
-//        return self.withUnsafeBytes { bytes in
-//            let cfdata = CFDataCreateWithBytesNoCopy(nil, bytes.baseAddress?.assumingMemoryBound(to: UInt8.self), bytes.count, kCFAllocatorNull)
-//            return ((cfdata as NSData?) as Data?) ?? Data()
-//        }
-//    }
-//}
+@available(iOS 13.0, *)
+@available(tvOS 13.0, *)
+@available(watchOS 6, *)
+extension SymmetricKey: CustomStringConvertible {
 
-//@available(iOS 13.0, *)
-//@available(tvOS 13.0, *)
-//@available(watchOS 6, *)
-//struct SymmetricKeyStore {
-//    
-//    /// Stores a CryptoKit key in the keychain as a generic password.
-//    func storeKey(_ key: SymmetricKey, account: String) -> SymmetricKey? {
-//
-//        // Treat the key data as a generic password.
-//        let query = [kSecClass: kSecClassGenericPassword,
-//                     kSecAttrAccount: account,
-//                     kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
-//                     kSecUseDataProtectionKeychain: true,
-//                     kSecValueData: key.rawRepresentation] as [String: Any]
-//        
-//        // Add the key data.
-//        _ = SecItemAdd(query as CFDictionary, nil)
-//        return readKey(account: account)
-//    }
-//    
-//    /// Reads a CryptoKit key from the keychain as a generic password.
-//    func readKey(account: String) -> SymmetricKey? {
-//
-//        // Seek a generic password with the given account.
-//        let query = [kSecClass: kSecClassGenericPassword,
-//                     kSecAttrAccount: account,
-//                     kSecUseDataProtectionKeychain: true,
-//                     kSecReturnData: true] as [String: Any]
-//        
-//        // Find and cast the result as data.
-//        var item: CFTypeRef?
-//        if SecItemCopyMatching(query as CFDictionary, &item) == errSecSuccess {
-//            if let optData = item as? Data {
-//                return SymmetricKey(data: optData)
-//            }
-//        }
-//        
-//        return nil
-//    }
-//}
+    public var description: String {
+        return self.rawRepresentation.withUnsafeBytes { bytes in
+            return "Key representation contains \(bytes.count) bytes."
+        }
+    }
+
+    var rawRepresentation: Data {
+        return self.withUnsafeBytes { bytes in
+            let cfdata = CFDataCreateWithBytesNoCopy(nil, bytes.baseAddress?.assumingMemoryBound(to: UInt8.self), bytes.count, kCFAllocatorNull)
+            return ((cfdata as NSData?) as Data?) ?? Data()
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+@available(tvOS 13.0, *)
+@available(watchOS 6, *)
+struct SymmetricKeyStore {
+
+    /// Stores a CryptoKit key in the keychain as a generic password.
+    func storeKey(_ key: SymmetricKey, account: String) -> SymmetricKey? {
+
+        // Treat the key data as a generic password.
+        let query = [kSecClass: kSecClassGenericPassword,
+                     kSecAttrAccount: account,
+                     kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
+                     kSecUseDataProtectionKeychain: true,
+                     kSecValueData: key.rawRepresentation] as [String: Any]
+
+        // Add the key data.
+        _ = SecItemAdd(query as CFDictionary, nil)
+        return readKey(account: account)
+    }
+
+    /// Reads a CryptoKit key from the keychain as a generic password.
+    func readKey(account: String) -> SymmetricKey? {
+
+        // Seek a generic password with the given account.
+        let query = [kSecClass: kSecClassGenericPassword,
+                     kSecAttrAccount: account,
+                     kSecUseDataProtectionKeychain: true,
+                     kSecReturnData: true] as [String: Any]
+
+        // Find and cast the result as data.
+        var item: CFTypeRef?
+        if SecItemCopyMatching(query as CFDictionary, &item) == errSecSuccess {
+            if let optData = item as? Data {
+                return SymmetricKey(data: optData)
+            }
+        }
+
+        return nil
+    }
+}
